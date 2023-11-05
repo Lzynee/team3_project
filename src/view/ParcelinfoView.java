7452b11c602b978fd70b0f0fcedbdc3beb47301d
kidchang93 <lck930721@gmail.com>
2023-11-05 20:09:29 +0900

    변경 내용(사이즈선택 if문, 변수선언, volume 출력값, 운송물정보확인 출력문)

--- src/view/ParcelinfoView.java
+++ src/view/ParcelinfoView.java
@@ -12,10 +12,14 @@ public class ParcelinfoView implements CommonView {
 	public void info(String userId,String parcelName) {
 
 		ParcelDao pdao = new ParcelDao();
-
-		int width = 0, length = 0, height = 0;
-		int cost = 0, mass = 0;
-
+		// 조건이 변경되었으므로 해당 변수들은 주석 처리함.
+//		int width = 0;
+//		int length = 0;
+//		int height = 0;
+		int cost = 0;
+		int mass = 0;
+		// 사이즈 선택 변수 초기화 및 선언
+		String smlSize = "";
 		String volume = "";
 
 		try {
@@ -25,10 +29,10 @@ public class ParcelinfoView implements CommonView {
 				System.out.println("-----------------------------------------------------");
 				System.out.println();
 				// 무게 => 수량 , 크기 => 사이즈로 문장 변경
-				System.out.println("                   [ 수량 및 사이즈 측정 ]");
+				System.out.println("               [ 수량 및 사이즈 입력 ]");
 				System.out.println();
 				// 무게 => 수량 , 크기 => 사이즈로 문장 변경
-				System.out.println(" 수량와 사이즈를 입력해 주세요");
+				System.out.println("              수량과 사이즈를 입력해 주세요");
 				System.out.println("-----------------------------------------------------");
 
 				// 수량별 요금 계산
@@ -51,33 +55,63 @@ public class ParcelinfoView implements CommonView {
 				cost = costs(mass);
 
 				// 사이즈 선택 과정
-				// 출력 문구 로직 변경 ( 이창규 )
-
+				// 출력 문구 로직 변경
+				// 조건 변경에 따른 if 문으로 로직 변경
+				// 수정 일시 : 2023.11.05
+				// 작성자 : 이창규
 				while (true) {
-					
-					System.out.println();
-					System.out.println("사이즈를 선택해 주세요.");
-					System.out.println();
-					System.out.print("가로(cm) : ");
-					width = Integer.parseInt(scan.nextLine());
-					System.out.println();
-					System.out.print("세로(cm) : ");
-					length = Integer.parseInt(scan.nextLine());
-					System.out.println();
-					System.out.print("높이(cm) : ");
-					height = Integer.parseInt(scan.nextLine());
-					System.out.println();
 
-					if (width + length + height > 160 || width > 100 || length > 100 || height > 100) {
-						System.out.println("가능한 규격이 아닙니다.");
+					System.out.println();
+					System.out.println("사이즈를 골라 입력해 주세요.");
+					System.out.println();
+					System.out.print("S / M / L : ");
+					smlSize = scan.nextLine().toUpperCase();
+
+					if (smlSize.equals("S")) {
+						System.out.println("Small 사이즈 선택");
+						break;
+					} else if (smlSize.equals("M")) {
+						System.out.println("Medium 사이즈 선택");
+						break;
+					} else if (smlSize.equals("L")) {
+						System.out.println("Large 사이즈 선택");
+						break;
+					} else {
+						System.out.println("유효하지 않은 사이즈입니다. 다시 시도하세요.");
 						continue;
 					}
-					break;
+
+					  	// --------------------------------
+
+//					System.out.println();
+//					System.out.println("사이즈를 선택해 주세요.");
+//					System.out.println();
+//					System.out.print("가로(cm) : ");
+//					width = Integer.parseInt(scan.nextLine());
+//					System.out.println();
+//					System.out.print("세로(cm) : ");
+//					length = Integer.parseInt(scan.nextLine());
+//					System.out.println();
+//					System.out.print("높이(cm) : ");
+//					height = Integer.parseInt(scan.nextLine());
+//					System.out.println();
+//
+//					if (width + length + height > 160 || width > 100 || length > 100 || height > 100) {
+//						System.out.println("가능한 규격이 아닙니다.");
+//						continue;
+//
+						// --------------------------------
+
 				}
 			
 				// 크기 정의
-				volume = String.format("%d*%d*%d(cm)", width, length, height);
+				// volume = String.format("%d*%d*%d(cm)", width, length, height);
+				// 입력된 값에 맞게 출력문 수정
+				// 수정 일시 : 2023.11.05
+				// 작성자 : 이창규
+				volume = String.format("%s", smlSize);
 
+				// 번호 설정
 				int parcelNum = pdao.selectCountId() + 1;
 
 				// 입력값들 set
@@ -97,10 +131,13 @@ public class ParcelinfoView implements CommonView {
 				System.out.println();
 				System.out.println("-----------------------------------------------------");
 				System.out.println();
-				System.out.printf(" | 내용 : %s || 크기 : %s || 무게 : %d |\n", parcelName, volume, mass);
+				// 크기 => 사이즈
+				System.out.printf(" | 내용 : %s || 사이즈 : %s || 무게 : %d |\n", parcelName, volume, mass);
+				System.out.println();
+				System.out.println("-----------------------------------------------------");
 				System.out.println();
 				System.out.println("1. 받는 사람 정보 화면으로 2. 정보 다시 입력 3. 메인 메뉴로");
-				System.out.println("-----------------------------------------------------");
+				System.out.println();
 				System.out.print(" 메뉴 선택 : ");
 				String menuNo = scan.nextLine();
 
