5853e33b0136e797440e2cd77bc47df54eb7d4ad
Lzyne <raina2zen90@gmail.com>
2023-11-06 17:47:00 +0900

    시퀀스 틀 수정

--- src/view/WaybillViewOnEdit.java
+++ src/view/WaybillViewOnEdit.java
@@ -10,7 +10,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
 
-public class WaybillViewOnEdit implements CommonView  {
+public class WaybillViewOnEdit implements CommonView {
 
     private static WaybillViewOnEdit view = new WaybillViewOnEdit();
 
@@ -72,14 +72,13 @@ public class WaybillViewOnEdit implements CommonView  {
         SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd");
 
         Date date = wb.getRegDate();
-        if(date == null) {
+        if (date == null) {
             date = new Date();
 //		        String nowTime1 = sdf1.format(date);
         }
 
 //			택배회사 관련 변수 === (Nov.05. 이양진)
-        if(wb.getCompanyName() == null)
-        {
+        if (wb.getCompanyName() == null) {
             wb.setCompanyName(wDao.selectCompanyByName(wb.getCompanyCd()));
         }
 
@@ -90,19 +89,19 @@ public class WaybillViewOnEdit implements CommonView  {
         System.out.println();
         System.out.println("┌--------------------------------------------------------------------------┐");
 //			택배회사 관련 변수를 사용하는 라인 === (Nov.05. 이양진)
-        System.out.printf("| 주문번호 : %-52s |\n", wb.getWaybillNo() , wb.getCompanyName());
+        System.out.printf("| 주문번호 : %-52s |\n", wb.getWaybillNo(), wb.getCompanyName());
         System.out.println("|--------------------------------------------------------------------------|");
         System.out.printf("| 보내는사람 : %-20s  보내는사람 전화번호: %-16s |\n", Sname, Scp);
-        System.out.printf("| 보내는사람 주소 : %-50s | \n","( " + Szipcode+" ) " + Saddr + " " + SDetailAddr);
+        System.out.printf("| 보내는사람 주소 : %-50s | \n", "( " + Szipcode + " ) " + Saddr + " " + SDetailAddr);
         System.out.println("|--------------------------------------------------------------------------|");
         System.out.printf("| 받는사람 : %-20s    받는사람 전화번호: %-16s |\n", Rname, Rcp);
-        System.out.printf("| 받는사람 주소 : %-50s |\n","( " + Rzipcode + " ) " + Raddr + " " + RDetailAddr);
+        System.out.printf("| 받는사람 주소 : %-50s |\n", "( " + Rzipcode + " ) " + Raddr + " " + RDetailAddr);
         System.out.println("|--------------------------------------------------------------------------|");
 //			상품 크기가 상품명 옆에 표시되도록 위치 이동 및 상품 무게 표시 라인 주석 처리 === (Nov. 05. 이양진)
-        System.out.printf("| 상품명 : %-50s |\n", pName, "(" +  pSize + ")");
+        System.out.printf("| 상품명 : %-50s |\n", pName, "(" + pSize + ")");
 //			System.out.printf("| 상품 무게 : %-20d  \t   |\n"/*, pWeight*/, pSize);
         System.out.println("|--------------------------------------------------------------------------|");
-        System.out.printf("|  %-30s| 요금 : %-10s |  %-10s |\n", wb.getMsg(), wb.getTotalFee() ,sdf1.format(date) );
+        System.out.printf("|  %-30s| 요금 : %-10s |  %-10s |\n", wb.getMsg(), wb.getTotalFee(), sdf1.format(date));
         System.out.println("└--------------------------------------------------------------------------┘");
         System.out.println();
         System.out.println("                            이용해 주셔서 감사합니다.");
@@ -112,7 +111,7 @@ public class WaybillViewOnEdit implements CommonView  {
         System.out.print(" 메뉴 선택 : ");
         String menuNO = scan.nextLine();
 
-        if("1".equals(menuNO)) {
+        if ("1".equals(menuNO)) {
             System.out.println();
             System.out.println();
             System.out.println();
@@ -121,8 +120,8 @@ public class WaybillViewOnEdit implements CommonView  {
         }
     }
 
-    // 주문 내역 출력 기능 시작 === (Nov.05. 이양진)
-    public void wbList() {
+    // 주문 내역 출력 기능 구현 작업 시작 === (Nov.05. 이양진)
+    public void phList() {
 
         while (true) {
 
@@ -135,6 +134,8 @@ public class WaybillViewOnEdit implements CommonView  {
                 System.out.println();
                 System.out.println("-----------------------------------------------------");
                 System.out.println();
+                System.out.println("                    [ 주문 접수 조회 메뉴 ]");
+                System.out.println();
                 System.out.println("       해당 기능을 사용하시려면 로그인을 하셔야 합니다.");
                 System.out.println();
                 System.out.printf("\t%-20s\t%-20s\n", "1. 회원 로그인", "2. 비회원 로그인");
@@ -142,94 +143,89 @@ public class WaybillViewOnEdit implements CommonView  {
                 System.out.print("메뉴 선택 : ");
                 String menuNo = scan.nextLine();
 
-                // 영수증이 회원으로 접수 되었을때
+                // 회원 주문 내역 확인
                 if ("1".equals(menuNo)) {
+
                     String userId = userV.Login();
-                    PurchaseHistory ph = wbDao.selectById()
-
-                    if (userId.equals(ph.getUserId()) ) {
-
+                    // 로그인 성공
+                    if (userId != "fail") {
                         System.out.println("-----------------------------------------------------");
                         System.out.println();
-                        System.out.println("        로그인에 성공하셨습니다. 메뉴를 선택해 주십시오.");
+                        System.out.println("        해당 계정에서 주문하신 내역은 다음과 같습니다.");
                         System.out.println();
-                        System.out.println(" 1. 주문 내역 확인  2. 삭제");
+                        System.out.println("  개별 주문 상세 열람 시 배송현황 조회 및 주문 취소를 하실 수 있습니다.");
+                        System.out.println();
+                        System.out.println("-----------------------------------------------------");
+                        System.out.println();
+                        // 주문 내역을 출력할 출력 부분
+                        System.out.println();
+                        System.out.println("-----------------------------------------------------");
+                        System.out.print(" 주문 상세 : ");
+//                        String menuNo = scan.nextLine();
+
+                        // 주문 상세 조회 시
+                        System.out.println("-----------------------------------------------------");
+                        System.out.println();
+                        System.out.println("        해당 항목에 대해 처리할 작업을 선택해 주십시오.");
+                        System.out.println();
+                        System.out.println(" 1. 배송 현황 확인  2. 영수증 출력  3. 주문 취소");
+                        System.out.println("-----------------------------------------------------");
+                        System.out.print(" 메뉴 선택 : ");
+                        String menuNum = scan.nextLine();
+//
+//                        if ("1".equals(menuNum)) {
+//                            System.out.println("배송 현황 확인");
+//
+//                        } else if ("2".equals(menuNum)) {
+//                            waybillInfo(wb,pc);
+//                            break;
+//
+//                        } else if ("3".equals(menuNum)) {
+//                            System.out.println("주문 취소");
+//
+//                        } else {
+//                            System.out.println("다른 값을 입력한 경우");
+//
+//                        }
+
+                        // 로그인 실패
+                    } else {
+                        System.out.println("-----------------------------------------------------");
+                        System.out.println();
+                        System.out.println("                  로그인에 실패하셨습니다.");
+                        System.out.println();
+                        continue;
+                    }
+
+                // 비회원 로그인
+                } else if ("2".equals(menuNo)) {
+                    String nonusercp = userV.Non_userlogin();
+
+                    // 로그인 성공
+                    if (nonusercp != "fail") {
+                        System.out.println("-----------------------------------------------------");
+                        System.out.println();
+                        System.out.println("               비회원 로그인에 성공하셨습니다.");
+                        System.out.println();
+                        System.out.println(" 1. 배송 현황 확인  2. 영수증 출력  3. 주문 취소");
                         System.out.println("-----------------------------------------------------");
                         System.out.print(" 메뉴 선택 : ");
                         String menuNum = scan.nextLine();
 
-                        if ("1".equals(menuNum)) {
-                            waybillInfo(wb,pc);
-                            break;
-                        } else {
-                            wbDao.delete(wbNum);
+                        System.out.println("주문 내역 출력");
 
-                            System.out.println("-----------------------------------------------------");
-                            System.out.println();
-                            System.out.println("                 접수가 취소되었습니다.");
-                            System.out.println();
-                            System.out.println("-----------------------------------------------------");
-                            break;
-                        }
-//						회원 주문내역 조회 실패 시 안내 메시지 수정 === (Nov.05. 이양진)
-                    } else if (userId != "fail") {
-                        System.out.println("-----------------------------------------------------");
-                        System.out.println();
-                        System.out.println("               해당 상품을 구매한 내역을 찾을 수 없습니다.");
-                        System.out.println();
-                        System.out.println("-----------------------------------------------------");
-                        break;
+                    // 로그인 실패
                     } else {
                         System.out.println("-----------------------------------------------------");
                         System.out.println();
-                        System.out.println("               아이디 패스워드가 틀립니다.");
-                        System.out.println();
-                        System.out.println("-----------------------------------------------------");
-                        break;
+                        System.out.println("       비회원 로그인에 실패하셨습니다. 다시 시도해 주십시오");
+                        continue;
                     }
 
-//					비회원 주문내역 조회 시 출력 문구 수정 === (Nov.05. 이양진)
-//					전화번호 입력 조건('- 없이 입력하세요')을 삭제 === (Nov.05. 이양진)
-                } else { // 영수증이 비회원으로 접수 되었을때
-                    // 핸드폰 번호를 입력하고 해당 영수증번호와 핸드폰 번호가 같을 경우 채출력 및 접수 취소를 시킨다.
-                    System.out.println("-----------------------------------------------------");
-                    System.out.println();
-                    System.out.println("해당 기능을 사용하시려면 해당 상품을 구매할 때 사용하신 전화번호를 입력하셔야 합니다.");
-                    System.out.println();
-                    System.out.println("-----------------------------------------------------");
-                    System.out.print("전화 번호 입력 : ");
-                    String inputNonCp = scan.nextLine();
-                    if (inputNonCp.equals(wb.getNonCp())) {
-                        System.out.println();
-                        System.out.println("-----------------------------------------------------");
-                        System.out.println();
-                        System.out.println("        비회원 로그인에 성공하셨습니다. 메뉴를 선택해 주십시오.");
-                        System.out.println();
-                        System.out.println(" 1. 재출력  2. 삭제");
-                        System.out.println("-----------------------------------------------------");
-                        System.out.print(" 메뉴 선택 : ");
-                        String menuNum = scan.nextLine();
+                // 1, 2 외 다른 값을 입력한 경우
+                } else {
+                    System.out.println("잘못된 접근");
 
-                        if ("1".equals(menuNum)) {
-                            waybillInfo(wb,pc);
-                            break;
-                        } else {
-                            wbDao.delete(wbNum);
-                            System.out.println("-----------------------------------------------------");
-                            System.out.println();
-                            System.out.println("                  접수가 취소되었습니다.");
-                            System.out.println();
-                            System.out.println("-----------------------------------------------------");
-                            break;
-                        }
-                    } else {
-                        System.out.println("-----------------------------------------------------");
-                        System.out.println();
-                        System.out.println("               전화번호가 일치하지 않습니다.");
-                        System.out.println();
-                        System.out.println("-----------------------------------------------------");
-                        break;
-                    }
                 }
 
             } catch (Exception e) {
@@ -237,15 +233,116 @@ public class WaybillViewOnEdit implements CommonView  {
 
                 System.out.println("다시 시도해 주십시오");
                 break;
+
             }
 
-        }
-    }
-
-    public static WaybillViewOnEdit getinstance()
-    {
-        return view;
-    }
-
-
+        }  // while
+    }  // phList()
 }
+
+//                if ("1".equals(menuNo)) {
+//                    String userId = userV.Login();
+//                    PurchaseHistory ph = wbDao.selectById();
+//
+//                    if (userId.equals(ph.getUserId()) ) {
+//
+//                        System.out.println("-----------------------------------------------------");
+//                        System.out.println();
+//                        System.out.println("        로그인에 성공하셨습니다. 메뉴를 선택해 주십시오.");
+//                        System.out.println();
+//                        System.out.println(" 1. 주문 내역 확인  2. 삭제");
+//                        System.out.println("-----------------------------------------------------");
+//                        System.out.print(" 메뉴 선택 : ");
+//                        String menuNum = scan.nextLine();
+//
+//                        if ("1".equals(menuNum)) {
+//                            waybillInfo(wb,pc);
+//                            break;
+//                        } else {
+//                            wbDao.delete(wbNum);
+//
+//                            System.out.println("-----------------------------------------------------");
+//                            System.out.println();
+//                            System.out.println("                 접수가 취소되었습니다.");
+//                            System.out.println();
+//                            System.out.println("-----------------------------------------------------");
+//                            break;
+//                        }
+////						회원 주문내역 조회 실패 시 안내 메시지 수정 === (Nov.05. 이양진)
+//                    } else if (userId != "fail") {
+//                        System.out.println("-----------------------------------------------------");
+//                        System.out.println();
+//                        System.out.println("               해당 상품을 구매한 내역을 찾을 수 없습니다.");
+//                        System.out.println();
+//                        System.out.println("-----------------------------------------------------");
+//                        break;
+//                    } else {
+//                        System.out.println("-----------------------------------------------------");
+//                        System.out.println();
+//                        System.out.println("               아이디 패스워드가 틀립니다.");
+//                        System.out.println();
+//                        System.out.println("-----------------------------------------------------");
+//                        break;
+//                    }
+//
+////					비회원 주문내역 조회 시 출력 문구 수정 === (Nov.05. 이양진)
+////					전화번호 입력 조건('- 없이 입력하세요')을 삭제 === (Nov.05. 이양진)
+//                } else { // 영수증이 비회원으로 접수 되었을때
+//                    // 핸드폰 번호를 입력하고 해당 영수증번호와 핸드폰 번호가 같을 경우 재출력 및 접수 취소를 시킨다.
+//                    System.out.println("-----------------------------------------------------");
+//                    System.out.println();
+//                    System.out.println("해당 기능을 사용하시려면 해당 상품을 구매할 때 사용하신 전화번호를 입력하셔야 합니다.");
+//                    System.out.println();
+//                    System.out.println("-----------------------------------------------------");
+//                    System.out.print("전화 번호 입력 : ");
+//                    String inputNonCp = scan.nextLine();
+//                    if (inputNonCp.equals(wb.getNonCp())) {
+//                        System.out.println();
+//                        System.out.println("-----------------------------------------------------");
+//                        System.out.println();
+//                        System.out.println("        비회원 로그인에 성공하셨습니다. 메뉴를 선택해 주십시오.");
+//                        System.out.println();
+//                        System.out.println(" 1. 재출력  2. 삭제");
+//                        System.out.println("-----------------------------------------------------");
+//                        System.out.print(" 메뉴 선택 : ");
+//                        String menuNum = scan.nextLine();
+//
+//                        if ("1".equals(menuNum)) {
+//                            waybillInfo(wb,pc);
+//                            break;
+//                        } else {
+//                            wbDao.delete(wbNum);
+//                            System.out.println("-----------------------------------------------------");
+//                            System.out.println();
+//                            System.out.println("                  접수가 취소되었습니다.");
+//                            System.out.println();
+//                            System.out.println("-----------------------------------------------------");
+//                            break;
+//                        }
+//                    } else {
+//                        System.out.println("-----------------------------------------------------");
+//                        System.out.println();
+//                        System.out.println("               전화번호가 일치하지 않습니다.");
+//                        System.out.println();
+//                        System.out.println("-----------------------------------------------------");
+//                        break;
+//                    }
+//                }
+//
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//
+//                System.out.println("다시 시도해 주십시오");
+//                break;
+//            }
+//
+//        }
+//    }
+//
+//    public static WaybillViewOnEdit getinstance()
+//    {
+//        return view;
+//    }
+//
+//
+//}
